734160f8aaf130ea6baf279178b76d1fd978c021
Fanni Putnóczki <fanni.putnoczki@gmail.com>
2019-02-12 20:39:54 +0100

    ignore

--- IsaacVoiceControl/voice.py
+++
@@ -1,9 +0,0 @@
-import os
-
-#should I switch to espeak?
-def say(speech):
-    os.system('pico2wave -w temp.wav "' + speech+ '" ')
-    os.system('aplay -q temp.wav >/dev/null 2>&1')
-
-def generate():
-    pass
