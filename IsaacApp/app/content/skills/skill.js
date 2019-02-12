f4af7bf3e4e2f1fcc3e1f53b4e7f70ff2bd8dabd
Fanni Putnóczki <fanni.putnoczki@gmail.com>
2019-02-12 20:50:41 +0100

    ok

--- IsaacApp/app/content/skills/skill.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2tpbGwuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJza2lsbC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGludGVyZmFjZSBTa2lsbCB7XG5cdG5hbWU6IHN0cmluZyxcblx0dXJsOiBzdHJpbmcsXG5cdGVuYWJsZWQ6IGJvb2xlYW5cbn0iXX0=
